5e8d2b8e4e1589941c63c3c9f3e85b305ef3ed64
Ben-Hur <138973267+botlorien@users.noreply.github.com>
2024-02-24 16:02:56 -0400

    24-02-2024

--- tasks.py
+++ tasks.py
@@ -1,11 +1,12 @@
 from facebook import Facebook
 import dataprocessing as dp
 
-face = Facebook()
-face.init_browser()
+
 
 
 def get_facebook_ads():
+    face = Facebook()
+    face.init_browser()
     return dp.process_content_ads(face.get_facebook_ads())
 
 
